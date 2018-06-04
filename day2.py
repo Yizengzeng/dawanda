8b4adfb849e2f0062cf5912c232baf0a69473208
Yizengzeng <39868151+Yizengzeng@users.noreply.github.com>
2018-06-04 09:05:53 +0800

    Add files via upload

--- day2.py
+++ day2.py
@@ -6,14 +6,14 @@ Created on Sun Jun  3 09:50:01 2018
 """
 
 import urllib.request as a
-city_pinyin=input("请输入城市：")
+city_pinyin=input("欢迎使用全球城市气候查询系统\n请输入城市：")
 url='http://api.openweathermap.org/data/2.5/forecast?q={},cn&mode=json&lang=zh_cn&&APPID=6a67ed641c0fda8b69715c43518b6996'
 info=a.urlopen(url.format(city_pinyin)).read().decode('utf-8','ignor')
 #print(info)
 import json
 data=json.loads(info)
 
-menno=input("查看当前城市按1，其它数值查看其它城市：")
+menno=input("查看当前城市请按1，其它数值查看其它城市：")
 if menno=='1':
     print("七天天气情况")
     a=range(1,6)
@@ -26,7 +26,7 @@ if menno=='1':
            "气压：":data['list'][a]['main']['pressure']}
         print("{}\n天气:{}\n最低温度：{}\n最高温度：{}\n气压：{}".format(q["日期："],q["天气："],q["最低温度："],q["最高温度："],q["气压："]))
 else:
-    print("2.查看其它城市天气")
+    print("\n查看其它城市天气")
     city_pinyin=input("请输入城市：")
     a=range(1,6)
     for i in a:
@@ -37,4 +37,4 @@ else:
            "最高温度：":data['list'][a]['main']['temp_max'],
            "气压：":data['list'][a]['main']['pressure']}
         print("{}\n天气:{}\n最低温度：{}\n最高温度：{}\n气压：{}".format(q["日期："],q["天气："],q["最低温度："],q["最高温度："],q["气压："]))
-
+        
